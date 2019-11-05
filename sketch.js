0339f007efce420f489ddd394a0f5bea998d5f3e
lizzybrooks <lizzy.brooks@gmail.com>
2019-11-04 20:56:39 -0800

    resized

--- sketch.js
+++ sketch.js
@@ -23,6 +23,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+
   angleMode(DEGREES);
   rectMode(CENTER);
   let first = new SandGrain(width/2-60, 180,random(0,360),width/2-20,height-5)
@@ -46,7 +47,7 @@ function setup() {
   fill(207, 4, 63,fader);
   fill(130, 80, 57)
   textFont(myFont);
-  textSize(70);
+  textSize(50);
   text('Time  Material', width/2, height/4);
 
 
@@ -54,39 +55,41 @@ function setup() {
 
 
 
-  textSize(50);
-  text('next meeting',width/5,height/2);
-  text('prompt',width-width/5,height/2)
+  textSize(24);
+  text('next meeting',width/5,height/3+50);
+  text('prompt',width-width/5,height/3+50)
 
-  textFont(fontRegular, 22)
-  text(nextMeeting,width/5,height/2+40)
-  text(prompt,width-width/5,height/2+40)
+  textFont(fontRegular, 16)
+  text(nextMeeting,width/5,height/3+80)
+  text(prompt,width-width/5,height/3+80)
 
   //textAlign(RIGHT)
 
 
   textAlign(CENTER);
   textFont(specialFont)
-  textSize(20);
-  text('P', width/2-60, height/4-20);
+  textSize(15);
+  text('P', width/2-45, height/4-12);
 
 
 }
 
 function draw(){
+
     noStroke();
     textFont(specialFont)
-    textSize(20);
-    text('P', width/2-60, height/4-20);
+    textSize(15);
+    text('P', width/2-45, height/4-12);
 
 
     fill(240)
     rect(width/2, height-height/4+40,270,120)
-    rect(width/2,height/4+33,500,30)
+    rect(width/2,height/4+35,300,30)
     fill(130, 80, 57)
     textFont(fontRegular, 22)
     text(mission, width/2, height-height/4)
-    text(contact,width/2,height/4+40)
+    textFont(fontRegular, 12)
+    text(contact,width/2,height/4+30)
 
     if(frameCount % 700 == 0){
         let s = new SandGrain(width/2-60, 180,random(0,360),width/2+random(-50,50),height-random(10,50))
@@ -145,7 +148,7 @@ class SandGrain {
 
         fill(130, 80, 57)
         textFont(specialFont)
-        textSize(20);
+        textSize(16);
         text('P', this.x, this.y);
 
 
